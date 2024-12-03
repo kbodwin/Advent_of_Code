1f5c347efe5c1489f4b796d476e3bce6ef91b3d9
kbodwin <kelly@bodwin.us>
2024-12-03 12:24:40 -0800

    str_split instead of str_remove on D3

--- 2024/day3_boring.R
+++ 2024/day3_boring.R
@@ -5,8 +5,10 @@ library(tidyverse)
 
 dat <- readLines(here::here("2024", "day3_dat1.txt")) |> str_flatten()
 
-mat <- dat |> 
-  str_remove_all("don.t\\(\\).*?(do\\(\\)|$)") |>
+mat <- dat |>
+  str_split("do(?=(n't)?\\(\\))") |>
+  unlist() |>
+  str_subset("^n't", negate = TRUE) |>
   str_extract_all("(?<=mul\\()\\d{1,3},\\d{1,3}(?=\\))") |>
   unlist() |>
   str_split(",") |>
