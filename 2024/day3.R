648d072ab37b418f483b929f48c7b2a05248961c
kbodwin <kelly@bodwin.us>
2024-12-02 22:24:58 -0800

    2024 add

---
+++ 2024/day3.R
@@ -0,0 +1,29 @@
+### December 3rd
+
+#### Part One
+library(stringr)
+library(purrr)
+
+dat <- readLines("day3_dat1.txt")
+
+mul <- `*`
+
+dat |> 
+  str_extract_all("mul\\(\\d{1,3},\\d{1,3}\\)") |>
+  unlist() |>
+  map_dbl( ~eval(parse(text = .x))) |>
+  sum()
+
+
+#### Part Two
+
+dat |> 
+  str_c(collapse = "") |>
+  str_split("do(?=(n't)?\\(\\))") |>
+  unlist() |>
+  str_subset("^n't", negate = TRUE) |>
+  str_c(collapse = "") |>
+  str_extract_all("mul\\(\\d{1,3},\\d{1,3}\\)") |>
+  unlist() |>
+  map_dbl(~eval(parse(text = .x))) |>
+  sum()
